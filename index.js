897f584738b2e636aaeb749f45d1a5e777c119e9
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-12-10 09:21:28 -0300

    adding endpoints for delete and modify

--- index.js
+++ index.js
@@ -35,6 +35,15 @@ let persons = [
   }
 ]
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 app.get('/api/persons', (request, response) => {
   Phone.find({}).then(phones => {
@@ -53,12 +62,37 @@ app.get('/api/persons/:id', (req, res, next) => {
     })
     .catch(error => next(error))
 })
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
+  const updatedPerson = {
+    name,
+    number,
+  }
 
-  res.status(204).end()
+  Phone.findByIdAndUpdate(
+    req.params.id,
+    updatedPerson,
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updated => {
+      if (updated) {
+        res.json(updated)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Phone.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -87,8 +121,12 @@ app.get('/info', (req, res) => {
     `)
   })
 })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
